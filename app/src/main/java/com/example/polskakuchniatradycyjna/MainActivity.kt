918ca6746305d973110d859a1e3e6491e164687c
igor <igor.kowalczyk07@poczta.fm>
2025-12-10 22:13:03 +0100

    added BottomNavigationView

--- app/src/main/java/com/example/polskakuchniatradycyjna/MainActivity.kt
+++ app/src/main/java/com/example/polskakuchniatradycyjna/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.polskakuchniatradycyjna.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,5 +21,38 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.startFragment,
+                R.id.menuChoiceFragment,
+                R.id.summaryFragment
+            )
+        )
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.startFragment -> {
+                    navController.popBackStack(R.id.startFragment, false)
+                    true
+                }
+                R.id.menuChoiceFragment -> {
+                    navController.navigate(R.id.menuChoiceFragment)
+                    true
+                }
+                R.id.summaryFragment -> {
+                    navController.navigate(R.id.summaryFragment)
+                    true
+                }
+                else -> false
+            }
         }
+    }
     }
