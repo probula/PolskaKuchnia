ed3752094e6c90056728a3e4050a4179ff2ea0a9
igor <igor.kowalczyk07@poczta.fm>
2026-01-07 16:01:49 +0100

    added data handling for CustomMealFragment

--- app/src/main/java/com/example/polskakuchniatradycyjna/ui/CustomMealFragment.kt
+++ app/src/main/java/com/example/polskakuchniatradycyjna/ui/CustomMealFragment.kt
@@ -1,75 +1,48 @@
 package com.example.polskakuchniatradycyjna.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.polskakuchniatradycyjna.R
 import com.example.polskakuchniatradycyjna.databinding.FragmentCustomMealBinding
-import com.example.polskakuchniatradycyjna.databinding.FragmentMenuChoiceBinding
+import com.example.polskakuchniatradycyjna.viewmodel.OrderViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CustomMealFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CustomMealFragment : Fragment() {
 
     private var _binding: FragmentCustomMealBinding? = null
     private val binding get() = _binding!!
 
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val viewModel: OrderViewModel by activityViewModels()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentCustomMealBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        binding.summaryFragment.setOnClickListener {
 
-       binding.summaryFragment.setOnClickListener {
-           findNavController().navigate(R.id.action_customMealFragment_to_summaryFragment)
-       }
+            when (binding.customMealGroup.checkedRadioButtonId) {
+                R.id.skladnik1 -> viewModel.setMainDish("Składnik 1", 10.0)
+                R.id.skladnik2 -> viewModel.setMainDish("Składnik 2", 5.0)
+            }
+
+            viewModel.confirmPersonOrder()
+            findNavController().navigate(R.id.summaryFragment)
+        }
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CustomMealFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CustomMealFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
